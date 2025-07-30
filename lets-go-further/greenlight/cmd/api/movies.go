5b21e622f0f21b5672f271045abe1ac0121e67b1
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-07-31 01:04:52 +0800

    json validation setup

--- lets-go-further/greenlight/cmd/api/movies.go
+++ lets-go-further/greenlight/cmd/api/movies.go
@@ -6,18 +6,33 @@ import (
 	"time"
 
 	"greenlight.isaachome.net/internal/data"
+	"greenlight.isaachome.net/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	// Initialize a new Validator instance.
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
@@ -30,7 +45,7 @@ func (app *application) showMovieHanlder(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	move := data.Move{
+	move := data.Movie{
 		ID:        id,
 		CreatedAt: time.Now(),
 		Title:     "Inception",
